613f04a16f4bddda27586ac2979eb443a27f7ba0
Ishika Binage <ishikabinage2@gmail.com>
2026-04-29 12:20:56 +0530

    Added Metro Fare calculator project

---
+++ Day-05/metrocard/metro.py
@@ -0,0 +1,11 @@
+print("Metrocard Calculator(₹)")
+
+fare = 20
+
+rides = int(input("enter number of rides:"))
+
+
+total_cost = rides * fare
+
+print("\nCost per ride :₹{fare}")
+print(f"Total cost: ₹{total_cost}")
